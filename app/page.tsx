f7fdf7820baefd59c4b107f7872dbfa73389e087
Yaw Ofosu-Asare <E127943@Mac.lan>
2026-04-11 22:32:53 +1000

    Fix archive startup on root load

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,7 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import Script from "next/script";
 import PageShell from "@/src/components/layout/PageShell";
+import Script from "next/script";
 
 export default function Home() {
-  const [hasHash, setHasHash] = useState(false);
-
-  useEffect(() => {
-    if (window.location.hash) {
-      setHasHash(true);
-      return;
-    }
-
-    window.location.replace("/#/home");
-  }, []);
-
-  if (!hasHash) {
-    return null;
-  }
-
   return (
     <PageShell>
       <main id="app"></main>
